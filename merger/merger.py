87f70ff8ec7111e5e8c80b2d310cb7b292169321
Lauren O'Connell <oconnel6@tcd.ie>
2018-02-12 22:27:19 +0000

    Added merger script

--- merger/merger.py
+++ merger/merger.py
@@ -10,32 +10,35 @@ token = "&id=524901&APPID=0bf967b47593d3c2338685f71a747c86&units=metric"
 def get(params):
   return requests.get(base + params + token).json()
 
-#city=input("Please enter the name of the city")
-city = sys.argv[1]
-print(city)
+list=['dublin', 'carlow', 'cavan', 'cork', 'donegal', 'galway', 'roscommon', 'tipperary' ]
 
-weather = get("q=" + city)
+for x in range (0,len(list)):
+    city = list[x]
+    print("Running for " + city)
 
-print(weather)
+    weather = get("q=" + city)
+    
+    #print(weather)
 
-lat = weather["coord"]["lat"]
-lon = weather["coord"]["lon"]
-weather_main = weather["weather"][0]["main"]
-weather_main = weather["weather"][0]["description"]
-temp = weather["main"]["temp"]
-pressure = weather["main"]["pressure"]
-humidity = weather["main"]["humidity"]
-wind_speed = weather["wind"]["speed"]
-clouds = weather["clouds"]["all"]
+    lat = weather["coord"]["lat"]
+    lon = weather["coord"]["lon"]
+    weather_main = weather["weather"][0]["main"]
+    weather_main = weather["weather"][0]["description"]
+    temp = weather["main"]["temp"]
+    pressure = weather["main"]["pressure"]
+    humidity = weather["main"]["humidity"]
+    wind_speed = weather["wind"]["speed"]
+    clouds = weather["clouds"]["all"]
 
-today = datetime.date.today()
+    today = datetime.date.today()
 
-today = today.strftime('%d-%b-%y')
+    today = today.strftime('%d-%b-%y')
 
-nextRow = [today, temp, -1, pressure, wind_speed, humidity];
+    nextRow = [today, temp, -1, pressure, wind_speed, humidity];
+    
+    filename = city + ".csv"
 
-filename = city + ".csv"
-
-with open(filename, 'a') as f:
-    writer = csv.writer(f)
-    writer.writerow(nextRow)
+    with open(filename, 'a', newline='\n') as f:
+        writer = csv.writer(f)
+        writer.writerow(nextRow)
+            
